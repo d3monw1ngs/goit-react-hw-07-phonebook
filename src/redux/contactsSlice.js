490ddc52e4f84c5efae1c23e9babc6cd1578e803
d3monw1ngs <tezzielumsit87@gmail.com>
2024-07-25 11:52:09 +0800

    Update contactsSlice.js

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,15 +1,10 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const BASE_URL = "https://669e1f509a1bda36800576f3.mockapi.io/contacts";
+const BASE_URL = "https://669e1f509a1bda36800576f3.mockapi.io/contacts/";
 
 const initialState = {
-  items: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+  items: [],
   isLoading: false,
   error: null,
 };
